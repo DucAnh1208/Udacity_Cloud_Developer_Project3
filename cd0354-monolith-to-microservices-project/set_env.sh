e1144ccbdbd24271c8b37167c00c4055245f99c2
DESKTOP-4OJ25AF\DucAnh <nguyenducanh.cr7@gmail.com>
2023-11-08 00:20:09 +0700

    Update code

--- cd0354-monolith-to-microservices-project/set_env.sh
+++ cd0354-monolith-to-microservices-project/set_env.sh
@@ -2,7 +2,7 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
 export POSTGRES_PASSWORD=12345678
-export POSTGRES_HOST=localhost
+export POSTGRES_HOST=postgres.cemgabb2sywr.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
 export AWS_BUCKET=my-bucket-2673763
 export AWS_REGION=us-east-1
